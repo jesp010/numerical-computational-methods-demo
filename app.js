8afd502437750127365befa69cbc824033d064ef
Juan Enrique Solis Perla <juansolisosx@gmail.com>
2018-09-10 23:20:50 -0700

    Added new features

--- app.js
+++ app.js
@@ -2,27 +2,32 @@ let express = require("express");
 let app = express();
 let bisection = require("./methods/bisection");
 let bodyParser = require('body-parser');
-
+let initial_value, final_value;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
-	res.render("methods/bisection/bisection")
+	res.render("methods/bisection/bisection");
 });
 
 app.get("/start", function(req, res){
-	let bisectionResults = bisection.start();
-	console.log(bisectionResults);
+	let bisectionResults = bisection.start(initial_value, final_value);
 	res.render("methods/bisection/start", {results: bisectionResults});
+});
+
+app.post("/start", function(req, res){
+	initial_value = req.body.initial_value;
+	final_value = req.body.final_value;
+	res.redirect("/start");
 });
 
 app.get("/*", function(req, res){
 	res.send("Page not found");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
 	console.log("Server Has Started");
 });
 
--- methods/bisection.js
+++ methods/bisection.js
@@ -1,57 +1,78 @@
-function start(){
-		const STOP_CONDITION =  0.0000001;
-		return main(-400,300);
-		
-		function main (initial_value, final_value){
-			return bisection(initial_value, final_value);
-		}
+let now = require("performance-now");
 
-		function func(x){
-			
-			if (typeof(x) == 'number')
-			return  - 12 * (x * x * x * x * x) - 6.4 * (x * x * x) + 12;
-			
-			return "Not a number";
-		}
+function start(initial_value, final_value) {
+	const STOP_CONDITION = 0.0000001;
+	
+	//cast args to number
+	let iv = Number(initial_value);
+	let fv = Number(final_value);
 
-		function bisection(initial_value, final_value){
-			if (func(initial_value) * func(final_value) >= 0){
-				console.log("'initial_value' or 'final_value' not defined");
-				
-				return;
-			}
+	//performance test
+	let start = now();
+	let end = now();
+	let results = main(iv, fv);
+	results.time = ((end - start)/1000).toFixed(8);
 
-			let counter = 0;
-			let middle_point = initial_value;
-			console.log(`iv: ${initial_value}, fv ${final_value}, mp: ${middle_point}`);
-			let results = {iv: initial_value, fv: final_value, iterations: [], mp: middle_point, ivs: [], fvs: [], mps: [], root}
-			
-			while((final_value - initial_value) >= STOP_CONDITION) {
-			
-				//find middle point
-				middle_point = (initial_value + final_value) / 2;
+	//return results
+	return results;
 
-				//check if middle point is root
-				if (func(middle_point) == 0.0)
-					break;
-				else if (func(middle_point) * func(initial_value) < 0)
-					final_value = middle_point;
-				else
-					initial_value = middle_point;
-				
-				results.iterations[counter] = counter;
-				results.mps[counter] = middle_point;
-				results.ivs[counter] = initial_value;
-				results.fvs[counter] = final_value;
-				counter++;
-				console.log(`Counter: ${counter}, iv: ${initial_value}, fv: ${final_value}, mp: ${middle_point}`);
-			}
 
-			results.root = middle_point;
-			return results;
-		}
+
+	//------------------------------------------ functions: --------------------------------------------
+	//main func
+	function main(initial_value, final_value) {
+		return bisection(initial_value, final_value);
 	}
 
+	//return f(x) = -12x^5 - 6.4x^3 + 12 ecuation 
+	function func(x) {
+		if (typeof x == "number")
+			return -12 * (x * x * x * x * x) - 6.4 * (x * x * x) + 12;
+
+		return "Not a number";
+	}
+
+	//bisection logic
+	function bisection(initial_value, final_value) {
+		if (func(initial_value) * func(final_value) >= 0) {
+			console.log("'initial_value' or 'final_value' not defined");
+
+			return;
+		}
+
+		let counter = 0;
+		let middle_point = initial_value;
+		let results = {
+			iv: initial_value,
+			fv: final_value,
+			iterations: [],
+			mp: middle_point,
+			ivs: [],
+			fvs: [],
+			mps: [],
+			root
+		};
+
+		while (final_value - initial_value >= STOP_CONDITION) {
+			//find middle point
+			middle_point = (initial_value + final_value) / 2;
+
+			//check if middle point is root
+			if (func(middle_point) == 0.0) break;
+			else if (func(middle_point) * func(initial_value) < 0)
+				final_value = middle_point;
+			else initial_value = middle_point;
+
+			results.iterations[counter] = counter;
+			results.mps[counter] = middle_point;
+			results.ivs[counter] = initial_value;
+			results.fvs[counter] = final_value;
+			counter++;
+		}
+
+		results.root = middle_point;
+		return results;
+	}
+}
 
 module.exports.start = start;
-
